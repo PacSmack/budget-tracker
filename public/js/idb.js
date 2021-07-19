aa9763306d2cb38e720d75d3c90b896c3637b98b
Nathan da Silva <pacsmacck@gmail.com>
2021-07-19 12:31:23 -0700

    indexeddb added to the application

---
+++ public/js/idb.js
@@ -0,0 +1,24 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1)
+
+// check for database version changes
+request.onupgradeneeded = function(event) {
+    // save a reference to the databse
+    const db = event.target.result;
+    // create a table called `new_budget`, with auto incrementing primary key
+    db.createObjectStore(`new_budget`, { autoIncrement: true });
+};
+
+// upon success
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store
+    db = event.target.result;
+    if (navigator.onLine) {
+        //sendTransaction()
+    }
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+};
